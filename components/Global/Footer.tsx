de6ee645827a770e37af47c8774d446d15b1ae48
contentlywriters <faciocontentlywriters@gmail.com>
2026-06-28 14:42:17 +0530

    update...

--- components/Global/Footer.tsx
+++ components/Global/Footer.tsx
@@ -64,7 +64,7 @@ export default function Footer() {
                   rel="noopener noreferrer"
                   className="text-[#F3ECE2]/50 hover:text-[#F3ECE2] transition-colors text-sm"
                 >
-                  Youtube
+                  YouTube
                 </Link>
               </li>
 
